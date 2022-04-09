7549aedf0259e80aaf9d8128d227a42d2214df21
Cristian <cristian@telnyx.com>
2022-04-09 13:26:41 -0500

    feat: 2015 day 1 part 2

--- aoc_2015/src/main.rs
+++ aoc_2015/src/main.rs
@@ -2,18 +2,33 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-fn count_parenthesis(address: String) -> i32 {
-    let count = address
+fn calculate_delta(x: char) -> i32 {
+    match x {
+        '(' => 1,
+        ')' => -1,
+        _ => 0,
+    }
+}
+
+fn count_parenthesis(address: &String) -> i32 {
+    let count = &address
         .chars()
         .map(|x: char| -> i32 {
-            match x {
-                '(' => 1,
-                ')' => -1,
-                _ => 0,
-            }
+            calculate_delta(x)
         })
         .reduce(|a, b| a + b);
     count.unwrap()
+}
+
+fn find_basement_position(address: &String) -> u32 {
+    let mut current_address = 0;
+    for (position, value) in address.chars().enumerate() {
+        current_address += calculate_delta(value);
+        if current_address == -1 {
+            return (position+1) as u32
+        }
+    }
+    panic!("Never reached the basement");
 }
 
 fn main() {
@@ -30,31 +45,30 @@ fn main() {
         Ok(_) => (),
     }
 
-    println!("The final floor is: {}", count_parenthesis(test_str));
+    println!("The final floor is: {}", count_parenthesis(&test_str));
+    println!("The first basement entry position is: {}", find_basement_position(&test_str));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    macro_rules! test {
-        ($fn_name:ident, $test_str:expr, $expected_output:expr) => {
-
-            #[test]
-            fn $fn_name() {
-                    assert_eq!(count_parenthesis($test_str), $expected_output);
-            }
-        }
+    #[test]
+    fn test_count_parenthesis(){
+        assert_eq!(count_parenthesis(&String::from("(())")), 0);
+        assert_eq!(count_parenthesis(&String::from("()()")), 0);
+        assert_eq!(count_parenthesis(&String::from("(((")), 3);
+        assert_eq!(count_parenthesis(&String::from("(()(()(")), 3);
+        assert_eq!(count_parenthesis(&String::from("))(((((")), 3);
+        assert_eq!(count_parenthesis(&String::from("())")), -1);
+        assert_eq!(count_parenthesis(&String::from("))(")), -1);
+        assert_eq!(count_parenthesis(&String::from(")))")), -3);
+        assert_eq!(count_parenthesis(&String::from(")())())")), -3);
     }
 
-    test!(test_0_0, String::from("(())"), 0);
-    test!(test_0_1, String::from("()()"), 0);
-    test!(test_3_0, String::from("((("), 3);
-    test!(test_3_1, String::from("(()(()("), 3);
-    test!(test_3_2, String::from("))((((("), 3);
-    test!(test_minus_1_0, String::from("())"), -1);
-    test!(test_minus_1_1, String::from("))("), -1);
-    test!(test_minus_3_0, String::from(")))"), -3);
-    test!(test_minus_3_1, String::from(")())())"), -3);
-
+    #[test]
+    fn test_basement_position(){
+        assert_eq!(find_basement_position(&String::from(")")), 1);
+        assert_eq!(find_basement_position(&String::from("()())")), 5);
+    }
 }
